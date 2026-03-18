936f486ac76faee3f778f6c83709e68858042e25
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2026-03-19 00:18:55 +0530

    implement page with hero section and mapped book cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import HeroSection from '@/components/HeroSection'
+import BookCard from '@/components/BookCard'
+import {sampleBooks} from "@/lib/constants"
 
 const page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline" >BookPilot-AI</h1>
-    </div>
+    <main className="wrapper container" >
+      <HeroSection />
+
+      <div className="library-books-grid" >
+        {sampleBooks.map((book) => (
+          <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+        ))}
+      </div>
+
+    </main>
   )
 }
 
